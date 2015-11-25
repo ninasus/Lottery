9d36a78c4f5445cdf47963a6a612a3ee2660b24c
akon <gvakon@gmail.com>
2015-11-25 22:58:05 +0200

    frontend

--- Lotereya/App_Start/BundleConfig.cs
+++ Lotereya/App_Start/BundleConfig.cs
@@ -21,8 +21,7 @@ namespace Lotereya
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/bootstrap.css"));
         }
     }
 }
---
+++ Lotereya/Content/assets/js/default.js
@@ -0,0 +1,23 @@
+﻿var Main = function () {
+	var runGoTop = function () {
+        $('.gotop').on('click', function (e) {
+            $("html, body").animate({
+                scrollTop: 0
+            }, "slow");
+            e.preventDefault();
+        });
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > 200) {
+				$('.gotop').fadeIn();
+			} else {
+				$('.gotop').fadeOut();
+			}
+		});
+	};
+
+    return {
+        init: function () {
+			runGoTop();
+        }
+    };
+}();
