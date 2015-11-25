c783a28412076aa4557b2da7350cebabd8225ac7
ninasus <ninasus@gmail.com>
2015-11-25 17:11:56 +0200

    ----

---
+++ Lotereya/Models/Draw.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lotereya.Models
+{
+    public class Draw
+    {
+        private static Draw instance { get; set; }
+        public static int CountElement {get;set;}
+        public int Count { get; set; }
+        public int[] PriceElements { get; set; }
+        public List<Guid> Winners { get; set; }
+        
+        protected Draw()
+        {
+            PriceElements = new int[CountElement];
+        }
+
+        public static Draw Instance()
+        {
+            if (instance == null)
+                instance = new Draw();
+            return instance;
+        }
+    }
+}
