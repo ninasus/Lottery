80d467a7df488a6d504f5ef705530093d14235b4
rvruzych-trade-active <rvruzych@trade-active.com>
2015-12-07 17:09:33 +0200

    регистрация + авторизация

---
+++ Lotereya/App_Start/Startup.Auth.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Owin;
+using Owin;
+using Lotereya.Models;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.AspNet.Identity;
+
+//[assembly: OwinStartup(typeof(Lotereya.Startup))]
+
+namespace Lotereya
+{
+    public partial class Startup
+    {
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            // настраиваем контекст и менеджер
+            app.CreatePerOwinContext<ApplicationContext>(ApplicationContext.Create);
+            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Admin/Login"),
+            });
+
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+        }
+    }
+}
--- Lotereya/Startup.cs
+++ Lotereya/Startup.cs
@@ -4,10 +4,11 @@ using Microsoft.Owin;
 
 namespace Lotereya
 {
-    public class Startup
+    public partial class Startup
     {
         public void Configuration(IAppBuilder app)
         {
+            ConfigureAuth(app);
             // Any connection or hub wire up and configuration should go here
             app.MapSignalR();
         }
